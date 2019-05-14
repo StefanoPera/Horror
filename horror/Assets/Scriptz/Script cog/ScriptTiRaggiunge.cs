1555098b562b196c9cce8af9712386c66a7e4b29
Mirko Ghisla <mirkoghisla01@gmail.com>
2019-05-14 22:20:09 +0200

    Jumpscare quasi finito

--- horror/Assets/Scriptz/Script cog/ScriptTiRaggiunge.cs
+++ horror/Assets/Scriptz/Script cog/ScriptTiRaggiunge.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//feat Mirko
 public class ScriptTiRaggiunge : MonoBehaviour
 {
     public  float distance;
     private float tuaMamma = 0.6f;
-    public GameObject cameraPlayer, cameraJumpScare;
+    public GameObject cameraPlayer, cameraJumpScare, canvasJumpScare;
+    public AudioSource jumpScare;   
 
     // Update is called once per frame
     void Update()
@@ -21,6 +23,8 @@ public class ScriptTiRaggiunge : MonoBehaviour
         {
             cameraPlayer.SetActive(false);
             cameraJumpScare.SetActive(true);
+            canvasJumpScare.SetActive(true);
+            jumpScare.Play();
         }
     }
 }
